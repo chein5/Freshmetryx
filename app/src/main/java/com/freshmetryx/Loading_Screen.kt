a3e75e5719d280623ab418cec5d4811c9b76112c
Sergio Gabriel Rioseco Gibert <sergio.rioseco.gibert@gmail.com>
2023-09-24 03:17:39 -0300

    Loading Screen y Login Activity
    Only UI

---
+++ app/src/main/java/com/freshmetryx/Loading_Screen.kt
@@ -0,0 +1,11 @@
+package com.freshmetryx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Loading_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading_screen)
+    }
+}
---
+++ app/src/main/java/com/freshmetryx/Login_User.kt
@@ -0,0 +1,11 @@
+package com.freshmetryx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login_User : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_user)
+    }
+}
