0c294fbd85c49eb0dcd208aa9ca3c27e3f61d177
Sergio Gabriel Rioseco Gibert <sergio.rioseco.gibert@gmail.com>
2023-09-25 21:06:04 -0300

    Modificacion de Activity Scanner
    Se modifico el diseño de todas las ventanas para funcionar en un tamaño de PIXEL 2.
    
    - A la ventana Scanner se le agregaron los labels para asi recibir la informacion tras escanear el codigo de barra, se agrego igualmente el icono de imagen para a futuro cargar la imagen del producto

--- app/src/main/java/com/freshmetryx/Home.kt
+++ app/src/main/java/com/freshmetryx/Home.kt
@@ -1,12 +1,23 @@
 package com.freshmetryx
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_Freshmetryx);
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        //Botones
+        lateinit var btnQr : ImageButton
+        btnQr = findViewById(R.id.btnEscaner)
+        btnQr.setOnClickListener {
+            val intent = Intent(this, MainActivity ::class.java)
+            startActivity(intent)
+        }
     }
 }
