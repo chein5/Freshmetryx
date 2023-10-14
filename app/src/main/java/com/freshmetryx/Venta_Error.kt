1f0aae495f2017d69e083c15fc3f89e8039aea6a
Sergio Gabriel Rioseco Gibert <sergio.rioseco.gibert@gmail.com>
2023-10-13 23:08:46 -0300

    Se agrego todo el UI del modulo ventas
    Adjunto el enlace para ver el orden de las activitys
    
    https://www.canva.com/design/DAFxMS55rAg/aIZBRBq-E3yYdCkMVcPzow/edit?utm_content=DAFxMS55rAg&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton

---
+++ app/src/main/java/com/freshmetryx/Venta_Error.kt
@@ -0,0 +1,11 @@
+package com.freshmetryx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Venta_Error : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_venta_error)
+    }
+}
