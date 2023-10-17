3e3c4a18fcaefd526bf77f3fafb15c7d0cfa3b72
chein5 <mati25quezada@gmail.com>
2023-10-16 23:19:09 -0300

    se trabajo en venta
    mas venta

---
+++ app/src/main/java/com/freshmetryx/Clase_ayuda.kt
@@ -0,0 +1,4 @@
+package com.freshmetryx
+
+data class Clase_ayuda (var nombre_producto : String= "", var cantidad_producto : Long =0) {
+}
--- app/src/main/java/com/freshmetryx/Venta_Carrito.kt
+++ app/src/main/java/com/freshmetryx/Venta_Carrito.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
+import android.widget.ImageButton
 import android.widget.ListView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
@@ -23,9 +24,15 @@ class Venta_Carrito : AppCompatActivity() {
     val db = Firebase.firestore
     private lateinit var binding: ActivityVentaCarritoBinding
     lateinit var listView_carrito : ListView
+    lateinit var lista_ayuda: ArrayList<Clase_ayuda>
     lateinit var lista_productos : ArrayList<Producto>
     lateinit var lista_carrito : ArrayList<Carrito>
-    private lateinit var dic_carrito : MutableMap <String,Long>
+    lateinit var dic_carrito : MutableMap <String,Long>
+    lateinit var dic_precio : MutableMap <String,Long>
+    lateinit var carrito: Carrito
+
+    lateinit var btn_crearVenta : ImageButton
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_venta_carrito)
@@ -36,7 +43,16 @@ class Venta_Carrito : AppCompatActivity() {
         binding.btnScanqrCarrito.setOnClickListener{( initScanner())}
         lista_productos= ArrayList()
         lista_carrito= ArrayList()
+        lista_ayuda= ArrayList()
         dic_carrito = mutableMapOf()
+        dic_precio = mutableMapOf()
+        carrito = Carrito()
+        btn_crearVenta = findViewById(R.id.btn_confirmarVenta)
+        /*btn_crearVenta.setOnClickListener {
+
+        }
+
+         */
     }
 
 
@@ -49,6 +65,7 @@ class Venta_Carrito : AppCompatActivity() {
 
     /*Esta funcion nos permitira capturar el resultado del escaneo para mostrarlo con un toast
   primero verificamos si la variable recibio un resultado, para posteriormente mostrar el resultado entregado */
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
         if (result != null){
@@ -83,7 +100,7 @@ class Venta_Carrito : AppCompatActivity() {
                     // Mostrar los datos en los TextField
                     if (data != null) {
                         Log.d("CORRECTO","encontrado")
-                        // Añade más líneas para otros campos según sea necesario
+
                     }
                 } else {
                     Toast.makeText(this,"No se encontro el dato", Toast.LENGTH_LONG ).show()
@@ -97,67 +114,117 @@ class Venta_Carrito : AppCompatActivity() {
 
     }
 
-    fun obtenerProducto (id: String, datos : List<Producto>){
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun obtenerProducto(id: String, datos: List<Producto>) {
         val db = FirebaseFirestore.getInstance()
-        val docRef= db.collection("Productos").document(id)
+        val docRef = db.collection("Productos").document(id)
         docRef.get().addOnSuccessListener { documentSnapshot ->
-            if (documentSnapshot.exists()){
+            if (documentSnapshot.exists()) {
                 val nombre = documentSnapshot.getString("Nombre")
                 val stock = documentSnapshot.get("Stock")
                 val valor = documentSnapshot.get("Valor")
-                val producto= Producto(nombre.toString(), stock as Long, valor as Long)
-                Log.d("producto","$producto")
-                /*
-                * Aqui se busca el producto a traves del nombre en el diccionario, si se encuentra se suma 1 a la cantidad, si no lo encuentra lo agrega al diccionario
-                 */
-                if (dic_carrito.containsKey(nombre.toString())){
-                    val valor_actual= dic_carrito[nombre.toString()] ?: 0
-                    dic_carrito[nombre.toString()]= valor_actual+1
-                }else{
-                    dic_carrito.put(nombre.toString(),1)
-                }
-                /*
-                * Aqui se recorre el diccionario para agregar los productos a la lista del carrito
-                 */
-                for ((nombre, cantidad) in dic_carrito){
-                    Log.d("Se agrego","$datos")
-                    val carrito = Carrito(nombre, cantidad, valor)
-                    lista_carrito.add(carrito)
-                }
-                Log.d("diccionario","$dic_carrito")
-                lista_productos.add(producto)
-                llenarList(lista_carrito, nombre.toString())
+                val producto = Producto(nombre.toString(), stock as Long, valor as Long)
+                Log.d("producto", "$producto")
+                dic_precio= mutableMapOf(Pair (nombre.toString(),valor))
+                // Verificar si el producto ya está en la lista de ayuda
+                val carritoEnLista = lista_ayuda.find { it.nombre_producto == nombre.toString() }
 
-            }else{
-                Toast.makeText(this,"Producto no encontrado", Toast.LENGTH_SHORT ).show()
+                // Si ya está en la lista, actualizar solo la cantidad
+                if (carritoEnLista != null) {
+                    carritoEnLista.cantidad_producto++
+                } else {
+                    // Si no está en la lista, agregarlo
+                    val ayuda = Clase_ayuda(nombre.toString(), 1)
+                    lista_ayuda.add(ayuda)
+                }
+
+                llenarList(dic_carrito)
+
+
+
+            } else {
+                Toast.makeText(this, "Producto no encontrado", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    fun llenarList(lista_carrito: ArrayList<Carrito>, nombre_producto: String){
-        Log.e("LISTA CARRITO","$lista_productos")
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun llenarList(dic_carrito: MutableMap<String, Long>){
+        /*
+        * Aqui se recorre el diccionario para agregar los productos a la lista del carrito
+         */
+        for ((nombre, cantidad) in dic_carrito){
+            Log.d("Se agrego","$nombre")
+            val ayuda = Clase_ayuda(nombre, cantidad)
+            lista_ayuda.add(ayuda)
+        }
+        Log.d("diccionario","$dic_carrito")
+        Log.e("LISTA CARRITO","$lista_ayuda")
         listView_carrito= findViewById(R.id.list_carrito)
-        val adapter : ArrayAdapter<Carrito> = ArrayAdapter<Carrito>(
+        val adapter : ArrayAdapter<Clase_ayuda> = ArrayAdapter<Clase_ayuda>(
             this@Venta_Carrito,
             android.R.layout.simple_list_item_1,
-            lista_carrito as List<Carrito>
+            lista_ayuda as List<Clase_ayuda>
         )
         listView_carrito.adapter = adapter
         adapter.notifyDataSetChanged()
+        btn_crearVenta.setOnClickListener {
+            agregarDatos(dic_carrito)
+        }
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    fun agregarDatos(lista_carrito: ArrayList<Carrito>){
+    fun agregarDatos(dic_carrito: MutableMap<String, Long>){
+        val nombreProducto = dic_carrito["nombre_producto"].toString()
+        val cantidadProducto = dic_carrito["cantidad_producto"]!!
 
-        db.collection("Detalle_carrito").add(lista_carrito).addOnSuccessListener { documentReference ->
-            val nuevoId = documentReference.id
-            println("Se ha insertado un nuevo documento con el ID: $nuevoId")
-            //boleta = Boleta(LocalDate.now(),)
-            db.collection("Boleta").document(nuevoId).set(lista_carrito).addOnSuccessListener {
-                println("Se agrego la boleta")
+        var valorProducto: Long = 0 // Valor por defecto
+
+        for ((nombreC, cantidad ) in dic_carrito) {
+            for ((nombreP, precio) in dic_precio) {
+                if (nombreC == nombreP) {
+                    valorProducto = precio
+                    break // Se encontró el precio, salir del bucle 
+                }
             }
-        }.addOnFailureListener { e ->
-            println("Error al agregar el documento: $e")
         }
+
+
+
+        val carrito = Carrito(nombreProducto, cantidadProducto, valorProducto)
+        // Calcular los valores para la boleta
+        val subtotal = valorProducto * cantidadProducto
+        val iva = 0.19 // Supongamos un IVA del 19% (puedes cambiarlo según tu necesidad)
+        val total = subtotal * (1 + iva)
+        // Agregar el carrito a la colección "Detalle_carrito"
+        db.collection("Detalle_carrito").add(lista_carrito)
+            .addOnSuccessListener { documentReference ->
+                val nuevoId = documentReference.id
+                val boletaData = hashMapOf(
+                    "Cantidad_productos" to cantidadProducto,
+                    "IVA" to iva,
+                    "Ref_detalle" to nuevoId, // El ID generado al agregar el carrito
+                    "Subtotal" to subtotal,
+                    "Total" to total
+                )
+                db.collection("Boleta").add(boletaData)
+                    .addOnSuccessListener { documentReference ->
+                        println("Se ha insertado un nuevo documento de Boleta con el ID: ${documentReference.id}")
+                    }
+                    .addOnFailureListener { e ->
+                        println("Error al agregar el documento de Boleta: $e")
+                    }
+
+
+                println("Se ha insertado un nuevo documento con el ID: $nuevoId")
+            }
+            .addOnFailureListener { e ->
+                println("Error al agregar el documento: $e")
+            }
+
+
+        // Agregar los datos de la boleta a la colección "Boleta"
+
     }
 }
