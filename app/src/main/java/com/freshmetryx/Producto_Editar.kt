25bfaa291cd7f1b69f775eed609528d44dbfd4fc
Sergio Gabriel Rioseco Gibert <sergio.rioseco.gibert@gmail.com>
2023-11-11 01:12:17 -0300

    UI/UX Update
    Se agregaron las ventanas del modulo Inventario
    - Home
    - Agregar
    - Editar
    - Remover
    -Listado
    Ademas se agrego la ventana Listar Ventas

---
+++ app/src/main/java/com/freshmetryx/Producto_Editar.kt
@@ -0,0 +1,11 @@
+package com.freshmetryx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Producto_Editar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_producto_editar)
+    }
+}
---
+++ app/src/main/java/com/freshmetryx/Productos_Listar.kt
@@ -0,0 +1,11 @@
+package com.freshmetryx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Productos_Listar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_productos_listar)
+    }
+}
