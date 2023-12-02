74ae9b3273f87d8dde4da312b4651b8a66acfa98
Sergio Gabriel Rioseco Gibert <sergio.rioseco.gibert@gmail.com>
2023-12-02 02:12:36 -0300

    UI de Gestion
    Estan las ventanas de:
    
    - Menu Gestion
    - Gestion Negocio
    - Gestion Perfil
    - Gestion Membresia

---
+++ app/src/main/java/com/freshmetryx/Gestion_Perfil.kt
@@ -0,0 +1,11 @@
+package com.freshmetryx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Gestion_Perfil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gestion_perfil)
+    }
+}
